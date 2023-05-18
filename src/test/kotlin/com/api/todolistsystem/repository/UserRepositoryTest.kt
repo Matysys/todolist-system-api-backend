194e58cfc7ca0c5385d592050237a4f8ba6ef2e0
Matysys <matysys333@outlook.com>
2023-05-18 17:41:35 -0300

    Alterando o tipo de banco de dados

--- src/test/kotlin/com/api/todolistsystem/repository/UserRepositoryTest.kt
+++ src/test/kotlin/com/api/todolistsystem/repository/UserRepositoryTest.kt
@@ -1,11 +1,55 @@
 package com.api.todolistsystem.repository
 
+import com.api.todolistsystem.entity.UserEntity
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
 import org.springframework.test.context.ActiveProfiles
 
 @DataJpaTest
 @ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class UserRepositoryTest {
+
+    @Autowired lateinit var userRepository: UserRepository
+    @Autowired lateinit var testEntityManager: TestEntityManager
+
+    private lateinit var user: UserEntity
+
+    @BeforeEach
+    fun setup () {
+        user = testEntityManager.merge(buildUser())
+    }
+
+    @Test
+    fun `should find user by email`(){
+        //given
+        val email: String = "mateus@hotmail.com"
+
+        //when
+        val foundUser: UserEntity? = userRepository.findByEmail(email)
+
+        //then
+        Assertions.assertThat(foundUser).isNotNull
+        Assertions.assertThat(foundUser?.email).isEqualTo(email)
+        Assertions.assertThat(foundUser?.name).isEqualTo("Mateus")
+    }
+
+    private fun buildUser(
+        name: String = "Mateus",
+        email: String = "mateus@hotmail.com",
+        plainPassword: String = "mateus123",
+        id: Long = 1L
+    ) = UserEntity(
+        name = name,
+        email = email,
+        plainPassword = plainPassword,
+        todolist = mutableListOf(),
+        id = id
+    )
+
 }
